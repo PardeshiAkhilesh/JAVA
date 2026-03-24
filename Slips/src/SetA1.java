c8f983253c48a177ead01deea1635548fa8556dd
PardeshiAkhilesh <akhileshpardeshi3@gmail.com>
2026-03-24 17:16:54 +0530

    15-10-2025

--- Slips/src/SetA1.java
+++ Slips/src/SetA1.java
@@ -2,21 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class SetA1 {
-    public static void main(String args[]) throws IOException
-    {
+    public static void main(String args[]) throws IOException{
         String city;
-        ArrayList<String> a1 = new ArrayList<String>();
+        ArrayList<String> ar = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("How many cities :- ");
+        System.out.println("Enter the no of cities :- ");
         int n = sc.nextInt();
         for(int i=0;i<n;i++)
         {
-            System.out.println("Enter City Name :- ");
+            System.out.println("Enter CIty name :- ");
             city = sc.next();
-            a1.add(city);
+            ar.add(city);
         }
-        System.out.println(a1);
-        a1.clear();
-        System.out.println("After Remove :- "+a1);
+        System.out.println("City :- "+ar);
+        ar.clear();
+        System.out.println(ar);
     }
 }
